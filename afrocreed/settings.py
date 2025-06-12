6e143ad9dee79cb03ec38b0f67bb6ed641e88939
Austin <austinkanja05@gmail.com>
2025-06-12 03:05:44 +0300

    Updated settings.py

--- afrocreed/settings.py
+++ afrocreed/settings.py
@@ -35,8 +35,8 @@ DEBUG = True
 # ALLOWED_HOSTS = ['134b-105-160-62-18.ngrok-free.app']
 # CSRF_TRUSTED_ORIGINS = ['https://134b-105-160-62-18.ngrok-free.app']
 
-ALLOWED_HOSTS = ['web-production-73f99.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-73f99.up.railway.app/']
+ALLOWED_HOSTS = ['https://afrocreed.com','web-production-73f99.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://www.afrocreed.com','https://web-production-73f99.up.railway.app/']
 
 
 # Replace the above with your actual domain name in production
